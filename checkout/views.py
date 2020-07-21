ccf383248ca535bd96d2c550885cded327d57d9c
laurence100795 <laurence100795@gmail.com>
2020-07-21 16:47:55 +0000

    added stripe payments to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H7NrpEfDB6nIOBgPpSlLJ64jAe7j853XqDQZuiIkv1keEnkCKGIsbGtm5eOiS3PDmA5dkpjPnwgd2OiY7zoqdLJ00YsG5iAFK',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

aa828cb8cc337044ad489079c5d26f5bf1415202
laurence100795 <laurence100795@gmail.com>
2020-07-22 18:30:38 +0000

    added webhook handler

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+
+
+class StripeWH_handler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def hande_event(self, request):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
